d93289e00e0875a97074fdd28a75b0d763de93fb
mrartanis <mr.artanis@gmail.com>
2025-03-21 09:38:18 +0100

    Remove junk from build script

--- build_app_osx.sh
+++ build_app_osx.sh
@@ -3,14 +3,6 @@
 source env/bin/activate
 
 rm -rf build dist
-find . -type d -name "__pycache__" -exec rm -rf {} +
-find . -type f -name "*.pyc" -delete
-find . -type f -name "*.pyo" -delete
-find . -type f -name "*.pyd" -delete
-find . -type f -name ".DS_Store" -delete
-find . -type d -name "*.dist-info" -exec rm -rf {} +
-find . -type d -name "*.egg-info" -exec rm -rf {} +
-
 
 python3.11 setup.py py2app && echo "✅ all done"
 
