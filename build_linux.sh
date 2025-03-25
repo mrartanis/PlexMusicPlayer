a22495b14df54b7f129dc16c06e320cbd06d7de0
mrartanis <mr.artanis@gmail.com>
2025-03-25 16:03:56 +0100

    Add info for cross-compile for arm64

--- build_linux.sh
+++ build_linux.sh
@@ -7,3 +7,13 @@
 CC=clang-18 CXX=clang++-18 nuitka --onefile --plugin-enable=pyqt6 --show-progress plex_music_player/__main__.py || exit 1
 chmod +x __main__.bin
 mv __main__.bin PlexMusicPlayer_$(arch)
+
+
+# To build arm64 you should run it on arm machine or use docker with arm64 architecture
+# To prepare docker image run:
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# docker buildx create --use --name multiarch-builder || docker buildx use multiarch-builder
+# docker buildx inspect --bootstrap
+
+# Then you can build it with:
+# docker buildx build --platform linux/arm64 -t plexmusicplayer:latest .
