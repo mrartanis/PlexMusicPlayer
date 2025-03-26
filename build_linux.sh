0efb2170bda641064e3c0b747de46dcb2f9b4e03
mrartanis <mr.artanis@gmail.com>
2025-03-26 11:36:13 +0100

    Fix local build script

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,5 @@
-# Hey!
-# To build it you can use clang18, but you can use any compiler.
-# Also, packages python3.12-dev libpipewire-0.3-0 pipewire libxcb-cursor0 should be installed
-# patchelf should be > 0.12 you can use something like this: https://github.com/NixOS/patchelf/releases/download/0.18.0/patchelf-0.18.0-x86_64.tar.gz
-
-
-CC=clang-18 CXX=clang++-18 nuitka --onefile --plugin-enable=pyqt6 --show-progress plex_music_player/__main__.py || exit 1
-chmod +x __main__.bin
-mv __main__.bin PlexMusicPlayer_$(arch)
+pyinstaller -F --add-data "MusicApp.iconset/icon_256x256.png:icon" plex_music_player/__main__.py || exit 1
+mv dist/__main__ PlexMusicPlayer_linux_x86_64
 
 
 # To build arm64 you should run it on arm machine or use docker with arm64 architecture
