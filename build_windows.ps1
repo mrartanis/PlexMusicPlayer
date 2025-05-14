d5538416f9b95cf412f9deb7bfbbd61357384afa
mrartanis <mr.artanis@gmail.com>
2025-05-14 12:38:12 +0200

    Add new SVG icons to pyinstaller resources

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,2 +1,2 @@
 env\Scripts\Activate.ps1
-pyinstaller -i icon_win.ico  -F --paths=env/Lib/site-packages --add-data="MusicApp.iconset\icon_256x256.png:icon"  --windowed .\plex_music_player\__main__.py
+pyinstaller -i icon_win.ico  -F --paths=env/Lib/site-packages --add-data="MusicApp.iconset\icon_256x256.png:icon" --add-data="plex_music_player\icons_svg\*.svg;plex_music_player\icons_svg" --windowed .\plex_music_player\__main__.py
